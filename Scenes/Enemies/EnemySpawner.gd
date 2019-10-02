4f87569010673e4e76291ec03efdb2c1cff65a94
Derek <channelnumber52@gmail.com>
2019-10-02 15:14:48 -0400

    Added EnemySpawner Node

--- Scenes/Enemies/EnemySpawner.gd
+++ Scenes/Enemies/EnemySpawner.gd
@@ -1,13 +1,12 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const MAX_AMOUNT_OF_ENEMIES : int = 5
+export (PackedScene) var Enemy
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _ready() -> void:
+	pass
+
+
+func _process(delta) -> void:
+	pass
