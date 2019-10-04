f4d4823d9cc3bfd67a1ee10fd0910f0ab24b0ef6
marco2188 <marco21688@gmail.com>
2019-10-04 10:58:22 +0200

    Integrate EnemySpawen in Level1 scene

--- Scenes/Enemies/EnemySpawner.gd
+++ Scenes/Enemies/EnemySpawner.gd
@@ -4,6 +4,7 @@ const MAX_AMOUNT_OF_ENEMIES : int = 5
 
 export (PackedScene) var Enemy
 export (float) var spawnRate = 1.0
+export (float) var enemy_scale = 1.0
 
 var enemyCount : int = 0
 
@@ -30,7 +31,10 @@ func _on_SpawnTimer_timeout() -> void:
 
 func spawnEnemy() -> void:
 	var enemy = Enemy.instance()
+	enemy.scale = Vector2(enemy_scale,enemy_scale)
 	#example of how to add enemy to scene root.
-	enemy.global_position = Vector2(100 * (enemyCount + 1), 100 * (enemyCount + 1))
+	enemy.global_position = self.global_position + Vector2(0, 100 * enemyCount)
 	get_tree().root.add_child(enemy)
 	enemyCount += 1
+
+
