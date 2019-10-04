f5b0f7e257c7a9bf35d20d2dcdfbe6a72966455f
Karbb <andrea.rubino1990@gmail.com>
2019-10-04 20:09:44 +0200

    #8 Implement "fire bullet" mechanic (#37)

--- Scenes/Spaceship/Spaceship.gd
+++ Scenes/Spaceship/Spaceship.gd
@@ -4,8 +4,17 @@ export (PackedScene) var Command_Reference = load("res://Scenes/Command/Command.
 
 export (float) var speed : float = 200
 
+onready var bullet_scene = preload("res://Scenes/Bullet/Bullet.tscn")
+onready var bullet_container = get_node("Bullets")
+onready var gun_node = get_node("Guns")
+
+export var fire_rate : float = .5
+
+var next_shot = 0
+var guns_position = Vector2(100,35)
 
 func _ready():
+	
 	var W_Button = Command_Reference.instance()
 	W_Button.Command("W", KEY_W, "key")
 	$Inputs.add_child(W_Button)
@@ -21,12 +30,15 @@ func _ready():
 	var D_Button = Command_Reference.instance()
 	D_Button.Command("D", KEY_D, "key")
 	$Inputs.add_child(D_Button)
-
+	
+	var J_Button = Command_Reference.instance()
+	J_Button.Command("J", KEY_J, "key")
+	$Inputs.add_child(J_Button)	
 
 func _physics_process(d):
 	HandleMovement(d)
-
-
+	handle_shooting(d)
+	
 func HandleMovement(d):
 	var direction = Vector2(0, 0)
 	if getCommand("W").isPressed():
@@ -40,7 +52,25 @@ func HandleMovement(d):
 	
 	var collider = move_and_collide(direction * speed * d)
 
-
+func handle_shooting(d):
+	if getCommand("J").isPressed():
+		fire(d)
+	else:
+		decrease_next_shot_remaining_time(d)
+		
+func fire(d):
+	
+	if next_shot <= 0:
+		var bullet = bullet_scene.instance()
+		bullet.position = self.get_position() + guns_position
+		bullet_container.add_child(bullet)
+		next_shot = fire_rate
+	else:
+		decrease_next_shot_remaining_time(d)
+	
+func decrease_next_shot_remaining_time(d):
+	next_shot -= d
+	
 func getCommand(ID):
 	for cmd in $Inputs.get_children():
 		if cmd.ID == ID:
