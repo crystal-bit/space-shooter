3af01c27f76d3df268823df03216aece94f8a379
Marco-2188 <56113417+Marco-2188@users.noreply.github.com>
2019-10-05 15:57:52 +0200

    Integrate EnemySpawen in Level1 scene (#40)

--- Scenes/Gameplay/Level.gd
+++ Scenes/Gameplay/Level.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 
 func _ready():
+	randomize()
 	$ScoreHandler.connect("scoreChanged", self, "showPoints")
 	$ScoreHandler.connect("multiplierChanged", self, "showMultiplier")
 	$AddPoints.connect("pressed", self, "addShipDestroyedPoints")
 	$ResetMultiplier.connect("pressed", $ScoreHandler, "resetMultiplier")
+	set_spawn_position()
 
 
 func addShipDestroyedPoints():
@@ -19,3 +21,7 @@ func showPoints(points):
 func showMultiplier(multiplier):
 	$Score/Multiplier.text = str("x", multiplier)
 
+func set_spawn_position():
+	var viewport_size = get_viewport().size
+	$EnemySpawner.global_position = Vector2(viewport_size.x - 100, viewport_size.y/4 + viewport_size.y/2*randf())
+	
