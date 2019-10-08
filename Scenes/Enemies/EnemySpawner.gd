13e090541c9855bac9a364dc018c9be96602bc1b
Derek <channelnumber52@gmail.com>
2019-10-08 16:51:27 -0400

    Fix merge issues

--- Scenes/Enemies/EnemySpawner.gd
+++ Scenes/Enemies/EnemySpawner.gd
@@ -4,10 +4,7 @@ const MAX_AMOUNT_OF_ENEMIES : int = 5
 
 export (PackedScene) var Enemy
 export (float) var spawnRate = 1.0
-<<<<<<< HEAD
 export (float) var enemy_scale = 1.0
-=======
->>>>>>> EnemySpawner
 
 var enemyCount : int = 0
 
@@ -34,17 +31,9 @@ func _on_SpawnTimer_timeout() -> void:
 
 func spawnEnemy() -> void:
 	var enemy = Enemy.instance()
-<<<<<<< HEAD
 	enemy.scale = Vector2(enemy_scale,enemy_scale)
 	#example of how to add enemy to scene root.
 	enemy.global_position = self.global_position + Vector2(0, 100 * enemyCount)
 	get_tree().root.add_child(enemy)
 	enemyCount += 1
 
-
-=======
-	#example of how to add enemy to scene root.
-	enemy.global_position = Vector2(100 * (enemyCount + 1), 100 * (enemyCount + 1))
-	get_tree().root.add_child(enemy)
-	enemyCount += 1
->>>>>>> EnemySpawner
