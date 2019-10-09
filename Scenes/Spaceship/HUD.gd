f16a58cd8060e2b864c7941b34fa50a65c2ac09e
Davide Cristini <davcri91@gmail.com>
2019-10-09 19:51:08 +0200

    Update code style and remove some print statement

--- Scenes/Spaceship/HUD.gd
+++ Scenes/Spaceship/HUD.gd
@@ -2,20 +2,21 @@ extends Node2D
 
 signal game_over
 
-#X and Y coordinates for the first life icon
+# X and Y coordinates for the first life icon
 const iconPosX = 250
 const iconPosY = 250
-#the offset, to place the other life icons.
-#This number should be set to the width of the icon + a number of pixels to use as a gap between icons
+# The offset, to place the other life icons.
+# This number should be set to the width of the icon + a number of pixels to use as a gap between icons
 export (float) var iconOffset = 200
 
-#This HUD will draw 4 lives on screen, and then
-#give the player one more life that doesn't get a graphic
-#it'd be pretty easy to change this behavior
+# This HUD will draw 4 lives on screen, and then
+# give the player one more life that doesn't get a graphic
+# it'd be pretty easy to change this behavior
 export (int) var MAX_LIVES = 4
-var currentLifeCount : int#I'm fine with this variable being renamed
+var currentLifeCount : int
 
 var lifeIcons = {}
+
 
 func _ready() -> void:
 	currentLifeCount = MAX_LIVES
@@ -39,16 +40,15 @@ func remove_life() -> void:
 	else:
 		lifeIcons.get(currentLifeCount).visible = false
 		currentLifeCount = currentLifeCount - 1
-	print(currentLifeCount)
+
 
 func add_life() -> void:
 	if currentLifeCount == MAX_LIVES:
 		pass
 	else:
 		currentLifeCount = currentLifeCount + 1
-		lifeIcons.get(currentLifeCount).visible = true
-	
-	print(currentLifeCount)
+		lifeIcons.get(currentLifeCount).visible = true	
+
 
 func place_health_icons() -> void:
 	$Health_Icon.position = Vector2(iconPosX, iconPosY)
@@ -57,7 +57,6 @@ func place_health_icons() -> void:
 		var health_icon = $Health_Icon.duplicate()
 		health_icon.position = Vector2(iconPosX + (iconOffset * i), iconPosY)
 		add_child(health_icon)
-		#i+1 is used as the key here, so our health_icon's key
-		#will be equal to the remaining number of lives
+		# i+1 is used as the key here, so our health_icon's key
+		# will be equal to the remaining number of lives
 		lifeIcons[i+1] = health_icon
-		print(i)
