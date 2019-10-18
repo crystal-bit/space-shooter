13c2ab5f672733482170f242292b039a882c16e8
Umberto <khelthos@hotmail.it>
2019-10-18 15:09:31 +0200

    Added Powerup Spawner
    + Powerup scene
    + Added Powerup spawn in Enemy1
    + Placeholder Powerup icon (Temporary)

--- Scenes/Bullet/BulletEnemy.gd
+++ Scenes/Bullet/BulletEnemy.gd
@@ -5,10 +5,8 @@ export var speed = 1000
 func _process(delta):
 	check_if_on_screen()
 
-
 func _physics_process(delta):
 	move_local_x(-(speed * delta))
-
 
 func _on_Area2D_body_entered(collider):
 	if "Player" in collider.get_groups():
